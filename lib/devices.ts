0ba43a3f101d9107cb64c156ff760b8d3b32bbe4
rcooke-warwick <ryan@balena.io>
2022-03-03 14:48:32 +0000

    debug: wait longer before trying to flash
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- lib/devices.ts
+++ lib/devices.ts
@@ -377,7 +377,7 @@ export class BalenaFin extends DeviceInteractor {
 			console.log(`DEST::`);
 			console.log(dest);
 			if (dest instanceof Object) {
-				await Bluebird.delay(1000); // Wait 1s before trying to flash
+				await Bluebird.delay(5000); // Wait 1s before trying to flash
 				console.log('Flashing started...');
 				await this.testBot.flashToDisk(dest, stream);
 				console.log('Flashed!');
